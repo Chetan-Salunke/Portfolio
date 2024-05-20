18fd8e21c0888af578e9615b516f28c5af68c409
Chetan-Salunke <chetanbusalunke@gmail.com>
2024-05-21 01:16:02 +0530

    new-scrollbar-section

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
   
+
+  // arrow button
+  function scrollLeft() {
+    document.querySelector('.main1').scrollBy({
+        left: -200,
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight() {
+    document.querySelector('.main1').scrollBy({
+        left: 200,
+        behavior: 'smooth'
+    });
+}
